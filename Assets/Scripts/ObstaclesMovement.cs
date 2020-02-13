9c37284fa81b90b383375039a825cd88420f53df
MrLuigi002 <mrluigi002@gmail.com>
2020-02-13 20:06:24 +0100

    Clouds added

---
+++ Assets/Scripts/ObstaclesMovement.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstaclesMovement : MonoBehaviour
+{
+
+    public GameObject cloudObs;
+
+    public Rigidbody2D cloudRigidbody;
+
+    public float lateralSpeed = 1f;
+  
+    
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        //transform.position = transform.position + lateralSpeed * Time.deltaTime * Vector3.right;
+
+        cloudRigidbody.velocity = Vector2.right * lateralSpeed;
+    }
+}
