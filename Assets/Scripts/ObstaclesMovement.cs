6b16c2c3d726c8758d4c0a47a07fcc4b13f63d79
MrLuigi002 <mrluigi002@gmail.com>
2020-02-15 19:06:02 +0100

    Added Spawners

--- Assets/Scripts/ObstaclesMovement.cs
+++ Assets/Scripts/ObstaclesMovement.cs
@@ -10,11 +10,29 @@ public class ObstaclesMovement : MonoBehaviour
     public Rigidbody2D cloudRigidbody;
 
     public float lateralSpeed = 1f;
+    public float pushSpeed = 10f;
+
+    public enum Direction{left, right};
+    public Direction direction;
+
+    Vector2 movementDirection = new Vector2();
   
+    void Awake() 
+    {
+        if(direction.ToString() == "left")
+        {
+            movementDirection = Vector2.left;
+        }
+
+        else
+        {
+            movementDirection = Vector2.right;
+        }
+    }
     
     void Start()
     {
-        
+        Invoke("DestroyObject", 12f);
     }
 
     
@@ -22,6 +40,16 @@ public class ObstaclesMovement : MonoBehaviour
     {
         //transform.position = transform.position + lateralSpeed * Time.deltaTime * Vector3.right;
 
-        cloudRigidbody.velocity = Vector2.right * lateralSpeed;
+        cloudRigidbody.velocity = movementDirection * lateralSpeed;
+
+        if(Input.anyKey)
+        {
+            cloudRigidbody.velocity = Vector2.left * pushSpeed;
+        }
+    }
+
+    private void DestroyObject()
+    {
+        Destroy(gameObject);
     }
 }
---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    public GameObject obstacleOne;
+    public GameObject obstacleTwo;
+    public GameObject obstacleThree;
+
+    [Range(5,10)]
+    public int spawnRange = 6;
+
+    //public float randomRange = 2.5f;
+    
+    
+    void Start()
+    {
+        InvokeRepeating("SpawnObstacle", 0f, spawnRange);
+    }
+
+    void SpawnObstacle()
+    {
+        Vector2 spawnPosition = new Vector2();
+        spawnPosition.x = transform.position.x;
+        spawnPosition.y = Random.Range(-2, 1);
+
+        Instantiate(obstacleOne, spawnPosition, Quaternion.identity);
+    }
+    
+}
