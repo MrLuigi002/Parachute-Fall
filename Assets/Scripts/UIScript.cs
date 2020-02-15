d93815d84a9f22ab9b4f84ff9c50f6bec5572727
MrLuigi002 <mrluigi002@gmail.com>
2020-02-15 20:02:07 +0100

    Added score

---
+++ Assets/Scripts/UIScript.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIScript : MonoBehaviour
+{
+
+    public int score = 0;
+    public int highScore = 0;
+    public Text scoreText;
+    public Text highText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("AddScore", 0f, 1f);
+        highScore = PlayerPrefs.GetInt("highScore");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void AddScore()
+    {
+        score++;      
+
+        if(score > highScore)
+        {
+            PlayerPrefs.SetInt("highScore", score);
+        }
+
+        scoreText.text = "SCORE: " + score;
+        highText.text = "HIGH SCORE: " + highScore;
+    }
+}
